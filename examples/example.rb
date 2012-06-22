4848b6de3e728a2bc252d758887299615359b64a
tnakamura <griefworker@gmail.com>
2012-06-22 09:57:52 +0900

    examples 下にある不要なプラグインを削除。

--- examples/example.rb
+++ examples/example.rb
@@ -1,15 +1,13 @@
-pipeline :foobar do
-  use :foo, :name => "hoge"
-  use :bar, :suffix => "fuga"
-  use :bar, :suffix => "moga"
-  use :hoge
-end
+# coding: utf-8
+require "fragile/plugin/rss_input"
+require "fragile/plugin/proc_filter"
+require "fragile/plugin/console_output"
 
-pipeline :hogefuga do
-  retry_count 3
+pipeline :ruby_rss_feed_to_console do
+  retry_count 4
 
-  use :foo, :name => "aaa"
-  use :bar, :suffix => "bbb"
-  use :hoge 
+  use :rss_input, :url => "http://d.hatena.ne.jp/griefworker/rss"
+  use :proc_filter, :proc => lambda{|x| x[:title].include?("[Ruby]")}
+  use :console_output
 end
 

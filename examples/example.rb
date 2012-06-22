ef766de68385394b694b2577037911e770ea33fe
tnakamura <griefworker@gmail.com>
2012-06-22 21:56:59 +0900

    example を SelectFilter に対応させた。

--- examples/example.rb
+++ examples/example.rb
@@ -1,13 +1,13 @@
 # coding: utf-8
 require "fragile/plugin/rss_input"
-require "fragile/plugin/proc_filter"
+require "fragile/plugin/select_filter"
 require "fragile/plugin/console_output"
 
-pipeline :ruby_rss_feed_to_console do
+pipeline :sample do
   retry_count 4
 
   use :rss_input, :url => "http://d.hatena.ne.jp/griefworker/rss"
-  use :proc_filter, :proc => lambda{|x| x[:title].include?("[Ruby]")}
+  use :select_filter, :proc => lambda{|x| x[:title].include?("[Ruby]")}
   use :console_output
 end
 

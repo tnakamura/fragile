6d34b0db0171b296d7ba1c6b386b0a12da325558
tnakamura <griefworker@gmail.com>
2013-04-27 06:51:20 +0900

    spec_helper を require_relative で読み込む。

--- spec/pipeline_spec.rb
+++ spec/pipeline_spec.rb
@@ -1,5 +1,5 @@
 # coding: utf-8
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require_relative "spec_helper"
 
 describe Fragile::Pipeline do
   describe "#name" do
--- spec/plugin_manager_spec.rb
+++ spec/plugin_manager_spec.rb
@@ -1,5 +1,5 @@
 # coding: utf-8
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require_relative "spec_helper"
 require "fragile/plugin/console_output"
 
 class TestApp

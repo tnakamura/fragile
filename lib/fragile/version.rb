8d21d951d6d0f670d076bd50e7abb385e13a0057
tnakamura <griefworker@gmail.com>
2013-06-28 21:28:46 +0900

    バージョンを 0.0.5 に更新。

--- lib/fragile/version.rb
+++ lib/fragile/version.rb
@@ -2,10 +2,10 @@
 # Name::      Fragile
 # Author::    tnakamura <http://d.hatena.ne.jp/griefworker>
 # Created::   Jun 15, 2012
-# Updated::   Jun 23, 2012
+# Updated::   Jun 28, 2013
 # Copyright:: tnakamura Copyright (c) 2013
 # License::   Licensed under the MIT LICENSE.
 
 module Fragile
-  VERSION = "0.0.4"
+  VERSION = "0.0.5"
 end
